3e54e442ab3d0c5059b1f78f93b44ab3a5a70a17
gray6dev <gray6sky.dev@protonmail.com>
2025-09-19 20:13:42 +0200

    Create NewMain.java

---
+++ temas/tema01/src/main/java/org/hojas/hoja01/NewMain.java
@@ -0,0 +1,32 @@
+package org.hojas.hoja01;
+
+public class NewMain {
+    
+    /**
+     *  Ángel González Cortavitarte, DAW1
+     *  Ejercicio 1
+     */
+
+    public static void main(String[] args) {
+        boolean casado = true;
+        final int MAXIMO = 999999;
+        byte diasem = 1;
+        short diaanual = 300;
+        char sexo = 'M';
+        long miliseg = 1298332800000L;
+        double totalfactura = 10350.678;
+        long poblacion = 6775235741L;
+        
+        System.out.println("----- EJERCICIO DE VARIABLES Y TIPOS DE DATOS -----"
+                            + "   El valor de la variable casado es " + casado
+                            + "   El valor de la variable MAXIMO es " + MAXIMO
+                            + "   El valor de la variable diasem es " + diasem
+                            + "   El valor de la variable diaanual es " + diaanual
+                            + "   El valor de la variable sexo es " + sexo
+                            + "   El valor de la variable miliseg es" + miliseg
+                            + "   El valor de la variable totalfactura es " + totalfactura
+                            + "   El valor de la variable población es " + poblacion);
+        
+    }
+    
+}
